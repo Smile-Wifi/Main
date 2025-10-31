26217dad5c3c2a02299f7c8a5bf6900e46dd6175
Smile-Wifi <kaptenlanaja2024@gmail.com>
2025-10-31 12:49:41 +0800

    Add files via upload

---
+++ pages/root/smiletools/service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "multi-tool-v1";
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
