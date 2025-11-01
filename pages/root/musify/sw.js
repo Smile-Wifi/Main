badbf2f0d148b7604548264d581023f136e60870
Smile-Wifi <kaptenlanaja2024@gmail.com>
2025-11-01 11:21:01 +0800

    Update sw.js

--- pages/root/musify/sw.js
+++ pages/root/musify/sw.js
@@ -1,28 +1,35 @@
-const CACHE_NAME = 'tiktok-cache-v1';
-const OFFLINE_URLS = [
+const CACHE_NAME = 'musify-cache-v1';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(r => {
-      const clone = r.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      return r;
-    }).catch(() => caches.match('./index.html')))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  if (url.endsWith('.mp4') || url.endsWith('.webm') || url.endsWith('.mov')) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request).then(fRes => {
+        const resClone = fRes.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return fRes;
+      }))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
 });
