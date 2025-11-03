6f6f442c06c49a89fe2f8718197bb12f6bfa83f5
Smile-Wifi <kaptenlanaja2024@gmail.com>
2025-11-03 21:04:46 +0800

    Update sw.js

--- pages/root/sports/sw.js
+++ pages/root/sports/sw.js
@@ -1,3 +1,7 @@
-const CACHE = 'sports-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', 'index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const C='smile-v1';
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(C).then(c=>c.addAll(['/', '/index.html','/GIF/earth.gif','/GIF/Home.png','/GIF/mars.gif']))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
